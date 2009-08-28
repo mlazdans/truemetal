51203ac58202861cd4be5c04557d0c4da0c14e66
Martins Lazdans <marrtins@dqdp.net>
2009-08-28 03:09:05 +0000

    Right
    git-svn-id: https://svn.dqdp.net/truemetal@74 2400a693-290a-4e87-ad21-b279ed43806c

--- templates/tmpl.right.php
+++ templates/tmpl.right.php
@@ -1,12 +1,9 @@
 <div class="TD-cat">Aktuāli</div>
 <div class="List-item actual">
-	<a href="/article/582/" title="24.-25. jūlijs, piektdiena-sestdiena, &quot;Zirņu pagasta Dzilnās&quot;">ZVĒRĀ 2009</a>
+	<a href="/article/583/" title="28. augusts, piektdiena, klubs &quot;Melnā Piektdiena&quot;">MELNĀ IZPĀRDOŠANA</a>
 </div>
-<div class="List-item">
-	<a href="/article/579/" title="30. jūlijs - 1. augusts, ceturtdiena-sestdiena, &quot;Vācija, Vakena&quot;">WACKEN OPEN AIR 2009</a>
-</div>
-<div class="List-item">
-	<a href="/article/577/" title="6.-8. augusts, ceturdiena-sestdiena, &quot;Čehija&quot;">BRUTAL ASSAULT OPEN AIR 2009</a>
+<div class="List-item actual">
+	<a href="/article/584/" title="29. augusts, sestdiena, klubs &quot;Melnā Piektdiena&quot;">KONCERTSEZONAS ATKLĀŠANA</a>
 </div>
 <div class="List-item">
 	<a href="/article/581/" title="24. septembris, ceturtdiena, klubs &quot;Melnā Piektdiena&quot;">CHIMAIRA</a>
