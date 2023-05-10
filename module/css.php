88e3a4198b26b335805dc9f525c80842e2b7a356
Martins Lazdans <marrtins@dqdp.net>
2023-05-10 05:38:15 +0300

    Do not cache assets in debug mode

--- module/css.php
+++ module/css.php
@@ -1,31 +1,26 @@
-<?php
-// dqdp.net Web Engine v3.0
-//
-// contacts:
-// http://dqdp.net/
-// marrtins@dqdp.net
+<?php declare(strict_types = 1);
 
 $v = get('v');
-//$v = (int)array_shift($sys_parameters);
-if($v != $sys_script_version){
+
+if($v != $sys_script_version)
+{
 	header($_SERVER["SERVER_PROTOCOL"]." 410 Gone");
 	return;
 }
 
-if(
-	isset($_SERVER['HTTP_ACCEPT_ENCODING']) &&
-	substr_count($_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip')
-	)
+if(isset($_SERVER['HTTP_ACCEPT_ENCODING']) && substr_count($_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip'))
 {
 	ini_set('zlib.output_compression', 1);
 }
 
-$expires = (7 * 24 * 3600);
-header("Content-Type: text/css");
-header("Cache-Control: max-age=$expires");
-header("Expires: ".gmdate("D, d M Y H:i:s", time() + $expires) . " GMT");
+if(!$sys_debug)
+{
+	$expires = (7 * 24 * 3600);
+	header("Content-Type: text/css");
+	header("Cache-Control: max-age=$expires");
+	header("Expires: ".gmdate("D, d M Y H:i:s", time() + $expires) . " GMT");
+}
 
-//if($SCRIPTS = get('s')){
 if($SCRIPTS = $sys_css){
 	foreach($SCRIPTS as $js){
 		if(is_file("css/$js.css")){
@@ -33,4 +28,3 @@ if($SCRIPTS = $sys_css){
 		}
 	}
 }
-
