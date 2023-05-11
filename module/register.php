d902cbdada60298d69f58a2c4ffe2a74fd054ce5
Martins Lazdans <marrtins@dqdp.net>
2023-05-11 22:29:09 +0300

    Bit nicer msg

--- module/register.php
+++ module/register.php
@@ -1,13 +1,13 @@
 <?php declare(strict_types = 1);
 
-# Blacklisted
-if(user_blacklisted() || Logins::banned24h($GLOBALS['ip']))
-{
-	print "Blacklisted: $ip";
-	return;
-}
-
 $template = new MainModule($sys_module_id, 'index.tpl');
 $template->set_title("Reģistrācija");
 $template->set_right_defaults();
-$template->out(register($template, $sys_parameters));
+
+if(user_blacklisted() || Logins::banned24h($GLOBALS['ip']))
+{
+	$template->forbidden("Blacklisted IP: $ip");
+	$template->out(null);
+} else {
+	$template->out(register($template, $sys_parameters));
+}
