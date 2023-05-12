42751158eac0867d82a0f386cdabe4cb91aae5d3
Martins Lazdans <marrtins@dqdp.net>
2023-05-12 22:21:43 +0300

    Do not save empty session

--- lib/SessHandler.php
+++ lib/SessHandler.php
@@ -21,9 +21,14 @@ class SessHandler implements SessionHandlerInterface
 			return true;
 		}
 
-		$sql = "UPDATE logins SET l_sess_id = ?, l_sessiondata = ?, l_sess_ip = ?, l_lastaccess = CURRENT_TIMESTAMP, l_logedin = 'Y' WHERE l_id = ?";
-
-		return (bool)DB::Execute($sql, $id, $data, $ip, User::id());
+		if(empty($data))
+		{
+			$sql = "UPDATE logins SET l_sess_id = ?, l_sess_ip = ?, l_lastaccess = CURRENT_TIMESTAMP WHERE l_id = ?";
+			return DB::Execute($sql, $id, $ip, User::id());
+		} else {
+			$sql = "UPDATE logins SET l_sess_id = ?, l_sessiondata = ?, l_sess_ip = ?, l_lastaccess = CURRENT_TIMESTAMP, l_logedin = 'Y' WHERE l_id = ?";
+			return DB::Execute($sql, $id, $data, $ip, User::id());
+		}
 	}
 
 	public function read(string $id): string|false
@@ -45,7 +50,7 @@ class SessHandler implements SessionHandlerInterface
 
 	function destroy($sess_id): bool
 	{
-		return (bool)DB::Execute("UPDATE logins SET l_sess_id = NULL, l_logedin ='N' WHERE l_sess_id = ?", $sess_id);
+		return DB::Execute("UPDATE logins SET l_sess_id = NULL, l_logedin ='N' WHERE l_sess_id = ?", $sess_id);
 	}
 
 	function gc(int $max_lifetime): int|false
