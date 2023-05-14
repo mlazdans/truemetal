1c6bcfc0f99500b1463cd7542d1026729a8532e4
Martins Lazdans <marrtins@dqdp.net>
2023-05-14 09:50:33 +0300

    Add forum events

--- bin/mysql_procedures/view_mainpage.sql
+++ bin/mysql_procedures/view_mainpage.sql
@@ -10,14 +10,16 @@ CREATE OR REPLACE VIEW view_mainpage AS
 	r.res_entered,
 	r.table_id,
 	rm.res_comment_count,
-	rm.res_comment_last_date
+	rm.res_comment_last_date,
+	NULL as type_id
 FROM
 	`article` a
 JOIN `modules` m ON (a.art_modid = m.mod_id)
 JOIN `res` r ON r.`res_id` = a.`res_id`
 LEFT JOIN `res_meta` rm ON rm.`res_id` = r.`res_id`
 WHERE
-	r.res_visible = 1
+	r.res_visible = 1 AND
+	m.module_active = 1
 )
 UNION
 (SELECT
@@ -31,14 +33,40 @@ UNION
 	r.res_entered,
 	r.table_id,
 	rm.res_comment_count,
-	rm.res_comment_last_date
+	rm.res_comment_last_date,
+	NULL as type_id
 FROM
 	forum
 JOIN `modules` m ON (forum.forum_modid = m.mod_id)
 JOIN `res` r ON r.`res_id` = forum.`res_id`
 LEFT JOIN `res_meta` rm ON rm.`res_id` = r.`res_id`
 WHERE
-	r.res_visible = 1
+	r.res_visible = 1 AND
+	m.module_active = 1
+)
+UNION
+(SELECT
+	'forum' AS module_id,
+	r.res_id,
+	NULL AS art_id,
+	forum.forum_id,
+	r.res_name,
+	r.res_intro,
+	r.res_data,
+	r.res_entered,
+	r.table_id,
+	rm.res_comment_count,
+	rm.res_comment_last_date,
+	forum.type_id
+FROM
+	forum
+JOIN `res` r ON r.`res_id` = forum.`res_id`
+LEFT JOIN `res_meta` rm ON rm.`res_id` = r.`res_id`
+WHERE
+	r.res_visible = 1 AND
+	forum.type_id = 1 AND
+	-- TODO forum_modid NULL
+	forum.forum_modid = 0
 )
 ORDER BY
 	res_entered DESC;
