5453df5e3d4bfe68a999723584520d117c92574d
Martins Lazdans <marrtins@dqdp.net>
2023-05-20 09:14:19 +0300

    Add run time stats

--- include/console.php
+++ include/console.php
@@ -1,13 +1,13 @@
-<?php
-// dqdp.net Web Engine v3.0
-//
-// contacts:
-// http://dqdp.net/
-// marrtins@dqdp.net
-
-// Parametri konsoles skriptiem
+<?php declare(strict_types = 1);
 
 ini_set('error_prepend_string', '');
 ini_set('error_append_string', '');
 ini_set('html_errors', false);
 
+$CONS_START = microtime(true);
+
+register_shutdown_function(function(){
+	global $CONS_START;
+
+	print "\nFinished in: ".number_format(microtime(true) - $CONS_START, 3)."\n";
+});
