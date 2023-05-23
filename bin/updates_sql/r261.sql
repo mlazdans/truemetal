5c59bc1e667448403fb0944f16fa121e44a53814
Martins Lazdans <marrtins@dqdp.net>
2023-05-23 19:15:26 +0300

    Cleanup

--- bin/updates_sql/r261.sql
+++ bin/updates_sql/r261.sql
@@ -3,19 +3,22 @@
 SELECT * FROM logins WHERE l_nick IN (SELECT l_nick FROM `logins` GROUP BY l_nick HAVING COUNT(*)>1) ORDER BY l_lastaccess DESC;
 SELECT * FROM logins WHERE l_id IN (1651, 2079,1452, 1460,820, 264) ORDER BY l_lastaccess DESC;
 */
-DELETE FROM logins WHERE l_id IN (1651,819);
+DELETE FROM logins WHERE l_id IN (819);
+
 UPDATE attend SET l_id = 2079 WHERE l_id = 1651;
 UPDATE res SET login_id = 2079 WHERE login_id = 1651;
 UPDATE res_vote SET login_id = 2079 WHERE login_id = 1651;
 UPDATE comment_disabled SET login_id = 2079 WHERE login_id = 1651;
 UPDATE comment_disabled SET disable_login_id = 2079 WHERE disable_login_id = 1651;
 DELETE FROM logins WHERE l_id = 1651;
+
 UPDATE attend SET l_id = 1460 WHERE l_id = 1452;
 UPDATE res SET login_id = 1460 WHERE login_id = 1452;
 UPDATE res_vote SET login_id = 1460 WHERE login_id = 1452;
 UPDATE comment_disabled SET login_id = 1460 WHERE login_id = 1452;
 UPDATE comment_disabled SET disable_login_id = 1460 WHERE disable_login_id = 1452;
 DELETE FROM logins WHERE l_id = 1452;
+
 UPDATE attend SET l_id = 264 WHERE l_id = 820;
 UPDATE res SET login_id = 264 WHERE login_id = 820;
 UPDATE res_vote SET login_id = 264 WHERE login_id = 820;
