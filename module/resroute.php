423aa4f8cec930af34982961bbc2ac8e344223cc
Martins Lazdans <marrtins@dqdp.net>
2023-05-23 18:37:15 +0300

    Redirect moved

--- module/resroute.php
+++ module/resroute.php
@@ -3,9 +3,11 @@
 # TODO: vispār izvākt šo te moduli
 $res_id = (int)array_shift($sys_parameters);
 $c_id = (int)get('c_id');
+$moved = false;
 
 if(!($res = load_res($res_id))){
 	if($redirect_res = DB::ExecuteSingle("SELECT * FROM res_redirect WHERE from_res_id = ?", $res_id)){
+		$moved = true;
 		$res = $res = load_res($redirect_res['to_res_id']);
 		$c_id = 0;
 	}
@@ -14,7 +16,11 @@ if(!($res = load_res($res_id))){
 if($res)
 {
 	$location = $res->Route($c_id ? $c_id : null);
-	header("Location: $location");
+	if($moved){
+		redirectp($location);
+	} else {
+		redirect($location);
+	}
 } else {
 	$template = new MainModule("resroute");
 	$template->not_found();
