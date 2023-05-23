0758eba943a714b7baffa5b689c24315d40fa71e
Martins Lazdans <marrtins@dqdp.net>
2023-05-23 19:16:54 +0300

    Add r261 to update script

--- bin/apply_update-232.bat
+++ bin/apply_update-232.bat
@@ -11,3 +11,5 @@ mysql -uroot truemetal_remote < updates_sql\r235.sql
 call mysql_procedures/import_procedures.bat
 C:\php82\php.exe sess_update.php
 C:\php82\php.exe theme_collect_dup_comment.php > theme_comment_merged.log
+mysql -uroot truemetal_remote < updates_sql\r261.sql
+call mysql_procedures/import_procedures.bat
