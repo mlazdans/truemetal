a08caf3ba89b785e5c3ae597cd12e93cf1c64c80
Martins Lazdans <marrtins@dqdp.net>
2023-05-25 19:39:28 +0300

    Fix logins_update_meta

--- bin/mysql_procedures/PROC_logins_update_meta.sql
+++ bin/mysql_procedures/PROC_logins_update_meta.sql
@@ -3,27 +3,38 @@ DELIMITER $$
 DROP PROCEDURE IF EXISTS `logins_update_meta` $$
 CREATE PROCEDURE `logins_update_meta` (p_l_id INT)
 BEGIN
-	-- TODO: te skaita tikai komentu plus/minus, bet vajag visu res
-	--       comment_count skaitīt tikai komentus
 	UPDATE logins l
 	LEFT JOIN (
 		SELECT
 			r.login_id AS login_id,
-			SUM(CASE WHEN rv.rv_value = +1 THEN 1 ELSE 0 END) AS votes_plus,
-			SUM(CASE WHEN rv.rv_value = -1 THEN 1 ELSE 0 END) AS votes_minus,
-			COUNT(DISTINCT c.c_id) AS comment_count
+			SUM(rm.res_votes_plus_count) AS votes_plus,
+			SUM(rm.res_votes_minus_count) AS votes_minus
 		FROM res r
-		JOIN comment c ON c.res_id = r.res_id
-		LEFT JOIN res_vote rv ON rv.res_id = c.res_id
-		WHERE r.res_visible = 1 AND r.login_id IS NOT NULL AND r.login_id = COALESCE(p_l_id, r.login_id)
+		JOIN res_meta rm ON rm.res_id = r.res_id
+		WHERE r.login_id IS NOT NULL AND r.login_id = COALESCE(p_l_id, r.login_id)
 		GROUP BY r.login_id
 	) AS t2 ON t2.login_id = l.l_id
 	SET
-		l.votes_plus = t2.votes_plus,
-		l.votes_minus = t2.votes_minus,
-		l.comment_count = t2.comment_count
+		l.votes_plus = COALESCE(t2.votes_plus, 0),
+		l.votes_minus = COALESCE(t2.votes_minus, 0)
 	WHERE l.l_id = COALESCE(p_l_id, l.l_id)
 	;
+
+	UPDATE logins l
+	LEFT JOIN (
+		SELECT
+			r.login_id AS login_id,
+			COUNT(*) AS comment_count
+		FROM res r
+		JOIN res_meta rm ON rm.res_id = r.res_id
+		WHERE r.table_id = 3 AND r.res_visible = 1 AND r.login_id IS NOT NULL AND r.login_id = COALESCE(p_l_id, r.login_id)
+		GROUP BY r.login_id
+	) AS t2 ON t2.login_id = l.l_id
+	SET
+		l.comment_count = COALESCE(t2.comment_count, 0)
+	WHERE l.l_id = COALESCE(p_l_id, l.l_id)
+	;
+
 END $$
 
 DELIMITER ;
