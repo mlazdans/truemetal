52ff24f075abdd52cf937892d00c0318421b6268
Martins Lazdans <marrtins@dqdp.net>
2023-05-25 16:31:39 +0300

    Update upgrade script

--- bin/apply_update-232.bat
+++ bin/apply_update-232.bat
@@ -1,15 +1,21 @@
-@echo off
-chcp 437
-
 mysql -uroot truemetal_remote < updates_sql\r232-1.sql
 C:\php82\php.exe res_update.php
 mysql -uroot truemetal_remote < updates_sql\r232-2.sql
 mysql -uroot truemetal_remote < updates_sql\r232-3.sql
 mysql -uroot truemetal_remote < updates_sql\r232-4-res_merge.sql
-mysql -uroot truemetal_remote < updates_sql\r232-5-null.sql
+mysql -uroot truemetal_remote < updates_sql\r232-5-modifications.sql
 mysql -uroot truemetal_remote < updates_sql\r235.sql
-call mysql_procedures/import_procedures.bat
-C:\php82\php.exe sess_update.php
-C:\php82\php.exe theme_collect_dup_comment.php > theme_comment_merged.log
 mysql -uroot truemetal_remote < updates_sql\r261.sql
 call mysql_procedures/import_procedures.bat
+C:\php82\php.exe sess_update.php
+
+@REM CALL res_update_meta(NULL);
+@REM CALL logins_update_meta(NULL);
+
+exit
+
+C:\php82\php.exe theme_collect_dup_comment.php > theme_comment_merged.log
+call mysql_procedures/import_procedures.bat
+
+@REM CALL res_update_meta(NULL);
+@REM CALL logins_update_meta(NULL);
