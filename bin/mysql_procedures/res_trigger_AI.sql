a557a91cb1858662f76798ad6311f1e77bbbea03
Martins Lazdans <marrtins@dqdp.net>
2023-05-26 20:38:53 +0300

    Fix proc name

--- bin/mysql_procedures/res_trigger_AI.sql
+++ bin/mysql_procedures/res_trigger_AI.sql
@@ -3,9 +3,9 @@ DELIMITER $$
 DROP TRIGGER IF EXISTS res_trigger_AI $$
 CREATE TRIGGER res_trigger_AI AFTER INSERT ON res
 FOR EACH ROW BEGIN
-	CALL res_update_meta(NEW.res_id);
+	CALL res_meta_update(NEW.res_id);
 	IF NEW.res_resid IS NOT NULL THEN
-		CALL res_update_meta(NEW.res_resid);
+		CALL res_meta_update(NEW.res_resid);
 	END IF;
 END; $$
 
--- bin/mysql_procedures/res_trigger_AU.sql
+++ bin/mysql_procedures/res_trigger_AU.sql
@@ -3,15 +3,15 @@ DELIMITER $$
 DROP TRIGGER IF EXISTS res_trigger_AU $$
 CREATE TRIGGER res_trigger_AU AFTER UPDATE ON res
 FOR EACH ROW BEGIN
-	CALL res_update_meta(OLD.res_id);
+	CALL res_meta_update(OLD.res_id);
 	IF OLD.res_resid IS NOT NULL THEN
-		CALL res_update_meta(OLD.res_resid);
+		CALL res_meta_update(OLD.res_resid);
 	END IF;
 	IF OLD.res_id != NEW.res_id THEN
-		CALL res_update_meta(NEW.res_id);
+		CALL res_meta_update(NEW.res_id);
 	END IF;
 	IF OLD.res_resid != NEW.res_resid AND NEW.res_resid IS NOT NULL THEN
-		CALL res_update_meta(NEW.res_resid);
+		CALL res_meta_update(NEW.res_resid);
 	END IF;
 END; $$
 
