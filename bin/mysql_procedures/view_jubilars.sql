e175c3f5fbb1bb2d35ad31502c7744070799b7d0
Martins Lazdans <marrtins@dqdp.net>
2023-05-26 12:06:38 +0300

    Add NOTE

--- bin/mysql_procedures/view_jubilars.sql
+++ bin/mysql_procedures/view_jubilars.sql
@@ -2,6 +2,7 @@ CREATE OR REPLACE VIEW view_jubilars
 AS
 SELECT
 	DATE_FORMAT(l_entered, '%m%d') AS entered_stamp,
+	-- NOTE: ja entered ir 30-30 dec, tad vecums 1-2 jan var būt par gadu lielāks
 	CAST(DATE_FORMAT(CURRENT_TIMESTAMP, '%Y') - DATE_FORMAT(l_entered, '%Y') AS INT) AS age,
 	l_id,
 	l_nick,
