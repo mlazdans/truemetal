c90d7ee719db0f314afc3336ff20eef5b884fff1
Martins Lazdans <marrtins@dqdp.net>
2023-05-28 11:22:24 +0300

    Add res login_id NULL checks

--- bin/updates_sql/r261.sql
+++ bin/updates_sql/r261.sql
@@ -43,11 +43,36 @@ DELETE FROM logins WHERE l_id = 735;
 ALTER TABLE `logins` DROP INDEX `l_nick`;
 ALTER TABLE `logins` ADD UNIQUE(`l_nick`);
 
--- Update l_lastaccess pēc pēdējā komenta datuma
-UPDATE logins l SET l.l_lastaccess = (SELECT res.res_entered FROM res WHERE res.login_id = l.l_id AND res.table_id = 3 ORDER BY res.res_entered DESC LIMIT 1)
-WHERE l.l_lastaccess IS NULL AND l.comment_count > 0
-
 ALTER TABLE `logins`
 	CHANGE `l_nick` `l_nick` VARCHAR(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
 	CHANGE `l_login` `l_login` VARCHAR(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL
 ;
+
+-- Daži res bez login_id
+-- SELECT * FROM logins WHERE l_email IN (SELECT res_email FROM `res` WHERE table_id < 4 AND login_id IS NULL GROUP BY res_email)
+-- UNION
+-- SELECT * FROM logins WHERE l_nick IN (SELECT res_nickname FROM `res` WHERE table_id < 4 AND login_id IS NULL GROUP BY res_nickname)
+
+-- SELECT * FROM res WHERE table_id < 4 AND login_id IS NULL AND EXISTS (SELECT * FROM logins WHERE l_email = res_email)
+-- UNION
+-- SELECT * FROM res WHERE table_id < 4 AND login_id IS NULL AND EXISTS (SELECT * FROM logins WHERE l_nick = res_nickname)
+
+UPDATE res
+JOIN logins l ON l.l_email = res.res_email
+SET res.login_id = l.l_id
+WHERE
+	res.table_id < 4 AND
+	res.login_id IS NULL
+;
+
+UPDATE res
+JOIN logins l ON l.l_nick = res.res_nickname
+SET res.login_id = l.l_id
+WHERE
+	res.table_id < 4 AND
+	res.login_id IS NULL
+;
+
+-- Update l_lastaccess pēc pēdējā komenta datuma
+UPDATE logins l SET l.l_lastaccess = (SELECT res.res_entered FROM res WHERE res.login_id = l.l_id AND res.table_id = 3 ORDER BY res.res_entered DESC LIMIT 1)
+WHERE l.l_lastaccess IS NULL AND l.comment_count > 0
