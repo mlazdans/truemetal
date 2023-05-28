799456085e50e821afcec6bc6623df43000e099a
Martins Lazdans <marrtins@dqdp.net>
2023-05-28 17:32:31 +0300

    Add mem stats

--- include/console.php
+++ include/console.php
@@ -11,5 +11,6 @@ $CONS_START = microtime(true);
 register_shutdown_function(function(){
 	global $CONS_START;
 
-	print "\nFinished in: ".number_format(microtime(true) - $CONS_START, 3)."\n";
+	printf("\nMem usage: %s MB\n", number_format(memory_get_peak_usage(true)/1024/1204, 2));
+	printf("Finished in: %s sec\n", number_format(microtime(true) - $CONS_START, 2));
 });
