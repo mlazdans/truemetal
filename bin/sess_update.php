daafb9b8601630e16346032e13403988c37070eb
Martins Lazdans <marrtins@dqdp.net>
2023-05-28 03:03:06 +0300

    Get session data from logins table

--- bin/sess_update.php
+++ bin/sess_update.php
@@ -5,23 +5,42 @@ require_once('include/dbconnect.php');
 
 ini_set('session.use_cookies', false);
 
+register_shutdown_function(function(){
+	while(ob_get_level()){
+		print ob_get_clean();
+	}
+});
+
 session_start();
 
-$res = DB::withNewTrans(function(){
+$sess_ids = DB::withNewTrans(function(){
+	ob_start();
 	$allowed = ['forums', 'res'];
 	$p = DB::Prepare("UPDATE logins SET l_sess_id = ?, l_sessiondata = ? WHERE l_id = ?");
-	$q = DB::Query("SELECT * FROM sessions ORDER BY sess_lastaccess DESC");
+	// $q = DB::Query("SELECT * FROM sessions ORDER BY sess_lastaccess DESC");
+	$q = DB::Query("SELECT * FROM logins");
 	$sess_ids = [];
 	while($data = DB::Fetch($q))
 	{
-		session_decode($data['sess_data']);
-
-		// Ja nav l_id vai jau l_id agrāk redzēts, tad skip
-		$l_id = $_SESSION['login']['l_id'] ?? null;
-		if(!$l_id || isset($sess_ids[$l_id]))
-		{
+		if(empty($data['l_sessiondata'])){
 			continue;
 		}
+
+		if(!session_decode($data['l_sessiondata'])){
+			print_r($data);
+			session_start();
+			continue;
+			// return false;
+		}
+
+		$l_id = $data['l_id'];
+
+		// Ja nav l_id vai jau l_id agrāk redzēts, tad skip
+		// $l_id = $_SESSION['login']['l_id'] ?? null;
+		// if(!$l_id || isset($sess_ids[$l_id]))
+		// {
+		// 	continue;
+		// }
 
 		$sess_ids[$l_id] = true;
 
@@ -31,14 +50,16 @@ $res = DB::withNewTrans(function(){
 			}
 		}
 
-		DB::ExecutePrepared($p, $data['sess_id'], session_encode(), $l_id);
+		if(!DB::ExecutePrepared($p, $data['l_sess_id'], session_encode(), $l_id)){
+			return false;
+		}
 	}
 
-	return true;
+	return $sess_ids;
 });
 
-if($res){
-	print "Sessions updated\n";
+if($sess_ids){
+	printf("%d sessions updated\n", count($sess_ids));
 } else {
 	print "FAIL\n";
 }
