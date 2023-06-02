c96600e311bc90e9fa0820d71226760dca4690e1
Martins Lazdans <marrtins@dqdp.net>
2023-06-02 22:49:27 +0300

    Rename

--- lib/Entity/ViewResGdDataEntity.php
+++ lib/Entity/ViewResGdDataEntity.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types = 1);
 
-class ViewResGDDataEntity extends AbstractResEntity
+class ViewResGdDataEntity extends AbstractResEntity
 {
 	use ViewResGdDataEntityTrait;
 
--- lib/Entity/ViewResGdEntity.php
+++ lib/Entity/ViewResGdEntity.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types = 1);
 
-class ViewResGDEntity extends AbstractResEntity
+class ViewResGdEntity extends AbstractResEntity
 {
 	use ViewResGdEntityTrait;
 
