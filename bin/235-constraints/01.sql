44c00fc6518450ba78befb49de47ec827bc62eb7
Martins Lazdans <marrtins@dqdp.net>
2023-06-14 23:27:20 +0300

    Combine

--- bin/235-constraints/01.sql
+++ bin/235-constraints/01.sql
@@ -1,3 +1,4 @@
+-- LOGINS
 ALTER TABLE logins CHANGE l_disable_youtube l_disable_youtube TINYINT UNSIGNED NOT NULL DEFAULT 0;
 
 UPDATE logins SET l_forumsort_themes = 'T' WHERE l_forumsort_themes = '';
@@ -16,3 +17,12 @@ ALTER TABLE logins ADD CONSTRAINT l_logedin_check CHECK (l_logedin IN (0,1));
 ALTER TABLE logins ADD CONSTRAINT l_forumsort_themes_check CHECK (l_forumsort_themes IN (0,1));
 ALTER TABLE logins ADD CONSTRAINT l_forumsort_msg_check CHECK (l_forumsort_msg IN (0,1));
 ALTER TABLE logins ADD CONSTRAINT l_disable_youtube_check CHECK (l_disable_youtube IN (0,1));
+
+-- FORUM
+ALTER TABLE forum CHANGE forum_display forum_display TINYINT UNSIGNED NOT NULL DEFAULT 0;
+ALTER TABLE forum CHANGE type_id type_id TINYINT UNSIGNED NOT NULL DEFAULT 0;
+
+ALTER TABLE forum ADD CONSTRAINT forum_allow_childs_check CHECK (forum_allow_childs IN (0,1));
+ALTER TABLE forum ADD CONSTRAINT forum_closed_check CHECK (forum_closed IN (0,1));
+ALTER TABLE forum ADD CONSTRAINT forum_display_check CHECK (forum_display IN (0,1));
+ALTER TABLE forum ADD CONSTRAINT type_id_check CHECK (type_id IN (0,1));
--- bin/235-constraints/02.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE forum CHANGE forum_display forum_display TINYINT UNSIGNED NOT NULL DEFAULT 0;
-ALTER TABLE forum CHANGE type_id type_id TINYINT UNSIGNED NOT NULL DEFAULT 0;
-
-ALTER TABLE forum ADD CONSTRAINT forum_allow_childs_check CHECK (forum_allow_childs IN (0,1));
-ALTER TABLE forum ADD CONSTRAINT forum_closed_check CHECK (forum_closed IN (0,1));
-ALTER TABLE forum ADD CONSTRAINT forum_display_check CHECK (forum_display IN (0,1));
-ALTER TABLE forum ADD CONSTRAINT type_id_check CHECK (type_id IN (0,1));
