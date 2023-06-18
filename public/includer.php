c1a627ac2c46d996fd0b4b6d3ff4d3fc3f9c3db9
Martins Lazdans <marrtins@dqdp.net>
2023-06-18 21:57:15 +0300

    Add sys_module_id check

--- public/includer.php
+++ public/includer.php
@@ -1,5 +1,11 @@
 <?php declare(strict_types = 1);
 
+if(!isset($sys_module_id))
+{
+	header("Location: /");
+	return;
+}
+
 if(!in_array($sys_module_id, $sys_nosess_modules)){
 	require_once("$sys_root/include/session_handler.php");
 }
