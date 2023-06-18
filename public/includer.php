d98c2a3d69fa7142f1d3583ceb9dca36a067e9ae
Martins Lazdans <marrtins@dqdp.net>
2023-06-18 21:54:51 +0300

    Fix path

--- public/includer.php
+++ public/includer.php
@@ -1,7 +1,7 @@
 <?php declare(strict_types = 1);
 
 if(!in_array($sys_module_id, $sys_nosess_modules)){
-	require_once('include/session_handler.php');
+	require_once("$sys_root/include/session_handler.php");
 }
 
 if(file_exists("$sys_root/module/$sys_module_id.php")) {
