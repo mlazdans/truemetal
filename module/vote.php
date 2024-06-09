1a7008c256cd499dabf5c72050aa4a12b87e5ccb
Martins Lazdans <marrtins@dqdp.net>
2024-06-09 15:54:06 +0300

    Handle json

--- module/vote.php
+++ module/vote.php
@@ -7,4 +7,19 @@ $template = new MainTemplate();
 if($T = vote($template, $value, $res_id)){
 	$T->print();
 	return;
+} else {
+	# TODO: abstract out
+	if(isset($_GET['json'])){
+		header('Content-Type: text/javascript; charset='.$sys_encoding);
+
+		$template->set_out('container');
+
+		$jsonData = new StdClass;
+		$jsonData->title = "[ TRUEMETAL ".specialchars($template->get_title())." ]";
+		$jsonData->html = $template->parse();
+		print json_encode($jsonData);
+	} else {
+		$template->set_right_defaults();
+		$template->print();
+	}
 }
