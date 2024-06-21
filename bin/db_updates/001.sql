9714ab4219b84c38e6e34312bf01d6ddd77c6dbb
Martins Lazdans <marrtins@dqdp.net>
2024-06-21 13:39:11 +0300

    Add res_hash constraints

--- bin/db_updates/001.sql
+++ bin/db_updates/001.sql
@@ -1,9 +1,10 @@
-ALTER TABLE `res`
-	ADD `res_hash` VARCHAR(16) CHARACTER SET ascii COLLATE ascii_bin NOT NULL AFTER `res_id`;
+-- ALTER TABLE res DROP CONSTRAINT CHK_res_hash_len;
 
-UPDATE `res` SET res_hash = SHA2(CONCAT_WS('-', res_id, COALESCE(res_data)), 512);
+ALTER TABLE `res` ADD `res_hash` VARCHAR(16) CHARACTER SET ascii COLLATE ascii_bin NOT NULL AFTER `res_id`;
+UPDATE `res` SET `res_hash` = SHA2(CONCAT_WS('-', `res_id`, COALESCE(`res_data`)), 512);
 
 ALTER TABLE `res` ADD UNIQUE(`res_hash`);
+ALTER TABLE `res` ADD CONSTRAINT `CHK_res_hash_len` CHECK (LENGTH(`res_hash`) = 16);
 ALTER TABLE `res_history` ADD `res_name` TINYTEXT NULL DEFAULT NULL AFTER `res_data_compiled`;
 ALTER TABLE `res_history` ADD `doer_login_id` INT UNSIGNED NULL DEFAULT NULL AFTER `res_id`;
 ALTER TABLE `res_history` ADD FOREIGN KEY (`doer_login_id`) REFERENCES `logins`(`l_id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
